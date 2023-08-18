28e20b9bf71b3e759a74aab1d06b99d818c4b9ad
Mayuri.Suresh <Mayuri.Suresh@LDIGITBGL-L0203.godigit.corp>
2023-08-19 01:09:07 +0530

    first commit

---
+++ TestDemo1234/src/main/java/com/digit/TestDemo1234Application.java
@@ -0,0 +1,13 @@
+package com.digit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDemo1234Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDemo1234Application.class, args);
+	}
+
+}
